27defb2d9d32fcd7b9f6edd4a43b4c32c64535f7
UKM Seni Theatrisic <113005391+iannstronaut@users.noreply.github.com>
2024-03-20 22:39:53 +0700

    first commit

---
+++ src/main/java/piece/Main.java
@@ -0,0 +1,20 @@
+package piece;
+
+import chess.GamePanel;
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Chess 1vs1");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        
+        // Add GamePanel
+        GamePanel gp = new GamePanel();
+        window.add(gp);
+        window.pack();
+        
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
